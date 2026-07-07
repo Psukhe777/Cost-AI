6c27b45ac081eceff465376bef07f05e7d9fd84d
Nehemiah <nehemiahsturdivant@outlook.com>
2026-07-07 09:59:36 -0700

    Add queries export in queries.ts

---
+++ lib/queries.ts
@@ -0,0 +1,11 @@
+export { getDashboardData } from "@/lib/queries/dashboard";
+export {
+  getExplorerData,
+  getExplorerPageData
+} from "@/lib/queries/explorer";
+export {
+  getSafeMeData,
+  getShellUserData,
+  getUsageOverviewData
+} from "@/lib/queries/overview";
+export { getProjectsPageData } from "@/lib/queries/projects";
